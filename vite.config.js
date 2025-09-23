b1abba69a62849742c7ee560030d9b9e3ce2d42f
bassant2005 <basant.tareq.2005@gmail.com>
2025-09-23 18:01:05 +0300

    none

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-export default {
-  base: "/react-online-games-website/",
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/react-online-games-website/', // 👈 important for GitHub Pages
+  plugins: [react()],
+})
