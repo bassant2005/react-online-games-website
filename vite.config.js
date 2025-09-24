6f1ccfabe62046460fb54a85b6130a146c70f0d1
bassant2005 <basant.tareq.2005@gmail.com>
2025-09-24 22:38:23 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/react-online-games-website/",  // 👈 repo name here
   plugins: [react()],
 })
