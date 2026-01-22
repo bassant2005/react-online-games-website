51571fd98255fb2e4565148edde461bb862e2a7e
H P <basant.tareq.2005@gmail.com>
2026-01-22 15:24:33 +0200

    matching test

--- src/games/matching.jsx
+++ src/games/matching.jsx
@@ -115,11 +115,11 @@ function Matching() {
                         {hasWon ? "🎉 You matched all cards!" : "Flip two cards"}
                     </h3>
 
-                    <div className="row justify-content-center g-3">
+                    <div className="box  justify-content-center g-3">
                         {cards.map((card, index) => (
                             <div key={card.id} className="col-4 col-md-3 col-lg-2">
                                 <div
-                                    className={`card shadow text-center ${
+                                    className={`box  shadow text-center ${
                                         flipped.includes(index) || matched.includes(index)
                                             ? "border-success"
                                             : ""
@@ -127,7 +127,7 @@ function Matching() {
                                     style={{ cursor: "pointer" }}
                                     onClick={() => handleFlip(index)}
                                 >
-                                    <div className="card-body fs-2">
+                                    <div className="box  fs-2">
                                         {flipped.includes(index) || matched.includes(index) ? (
                                             symbolType === "faces" ? (
                                                 <i className={`fa-solid ${card.value}`}></i>
@@ -145,13 +145,16 @@ function Matching() {
 
                     {/* CONTROLS */}
                     <div className="mt-4 d-flex justify-content-center gap-3">
-                        <button className="btn btn-secondary" onClick={() => setLevel(null)}>
+                        <button className="btn" onClick={() => setLevel(null)}>
                             Change Level
                         </button>
-                        <button className="btn btn-primary" onClick={() => startGame(level)}>
+                        <button className="btn" onClick={() => setSymbolType(null)}>
+                            Change category
+                        </button>
+                        <button className="btn" onClick={() => startGame(level)}>
                             Restart
                         </button>
-                        <button className="btn btn-outline-danger" onClick={() => navigate("/")}>
+                        <button className="btn" onClick={() => navigate("/")}>
                             Leave
                         </button>
                     </div>
